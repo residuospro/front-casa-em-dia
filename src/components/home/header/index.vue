911612f24c6d16090f5e0ce1456a514b89d04aa3
kallif003 <kallifabrahao@gmail.com>
2026-07-13 16:29:10 -0300

    ajuste na home

--- src/components/home/header/index.vue
+++ src/components/home/header/index.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="flex flex-col items-start gap-2">
     <h1 class="text-3xl font-semibold">
-      Bem vindo, {{ perfil.nome }}! <span>👋</span>
+      {{ perfil.genero === "FEMININO" ? "Bem vinda" : "Bem vindo" }},
+      {{ perfil.nome }}! <span>👋</span>
     </h1>
     <span class="text-cinza_363637 text-base font-medium"
       >Que tal organizar sua rotina hoje ?</span
